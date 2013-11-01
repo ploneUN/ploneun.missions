bbf406e7122e1b0178e5c2579e057eb7a626bfa5
Izhar Firdaus <izhar@inigo-tech.com>
2013-11-02 00:36:32 +0800

    mission view now displaying properly (re #879)

--- ploneun/missions/browser/mission_view.py
+++ ploneun/missions/browser/mission_view.py
@@ -25,4 +25,71 @@ class Index(dexterity.DisplayForm):
         contentFilter['portal_type'] = 'ploneun.mission.missionreport'
 
         return bool(context.portal_catalog.queryCatalog(contentFilter))
-        
+
+    def detail_fields(self):
+        fields = []
+
+        members = []
+        mtool = self.context.portal_membership
+        for member in self.context.mission_members:
+            m = mtool.getMemberById(member)
+            members.append(
+                '"%s" &lt;%s&gt;' % (
+                    m.getProperty('fullname'),
+                    m.getProperty('email')
+                )
+            )
+
+        fields.append({
+            'id': 'members',
+            'title': 'Mission Members',
+            'render': ('<ul>%s</ul>' % ''.join(['<li>%s</li>' % i for i in (
+                members)])) if members else ''
+        })
+
+        toLocalizedTime = self.context.restrictedTraverse('@@plone').toLocalizedTime
+
+        fields.append({
+            'id': 'dates',
+            'title': 'When',
+            'render': '%s - %s' % (toLocalizedTime(self.context.startDate),
+                                    toLocalizedTime(self.context.endDate))
+        })
+
+        location = []
+        if self.context.mission_city:
+            location.append(self.context.mission_city)
+        location.append(self.context.country)
+
+        fields.append({
+            'id': 'location',
+            'title': 'Location',
+            'render': ', '.join(location)
+        })
+
+
+        contact = [self.context.contactName]
+
+        if self.context.contactEmail:
+            if  self.context.contactName:
+                contact = ['"%s"' % self.context.contactName]
+            contact.append('&lt;%s&gt;' % self.context.contactEmail)
+
+        if self.context.contactPhone:
+            contact.append('(%s)' % self.context.contactPhone)
+
+        if (''.join(contact)).strip():
+            fields.append({
+                'id': 'contact',
+                'title': 'Contact',
+                'render': ' '.join(contact)
+            })
+
+        if self.context.text:
+            fields.append({
+                'id': 'text',
+                'title': 'Note',
+                'render': self.context.text
+            })
+    
+        return fields
