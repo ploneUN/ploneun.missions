a10539e94a6158a29d7634a38b2ca3c5b31f4bc8
Izhar Firdaus <izhar@inigo-tech.com>
2013-11-05 14:56:17 +0800

    added country and ilo_themes indexers for missionreport (re #955)

--- ploneun/missions/indexer.py
+++ ploneun/missions/indexer.py
@@ -1,7 +1,15 @@
 from plone.indexer import indexer
 from five import grok
 from ploneun.missions.content.mission import IMission
+from ploneun.missions.content.missionreport import IMissionReport
 from DateTime import DateTime
+from Acquisition import aq_chain
+
+def get_mission(obj):
+    for i in aq_chain(obj):
+        if IMission.providedBy(i):
+            return i
+    return None
 
 @indexer(IMission)
 def mission_start(obj):
@@ -17,6 +25,12 @@ def mission_end(obj):
 def mission_country(obj):
     return obj.country
 
-@indexer(IMission)
-def mission_country(obj):
-    return obj.country
+@indexer(IMissionReport)
+def missionreport_country(obj):
+    mission = get_mission(obj)
+    return mission.country
+
+@indexer(IMissionReport)
+def missionreport_ilo_themes(obj):
+    mission = get_mission(obj)
+    return mission.ilo_themes
