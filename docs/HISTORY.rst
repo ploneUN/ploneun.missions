00df2907984f127113a3972e21e19c181254b6a7
Izhar Firdaus <izhar@inigo-tech.com>
2013-11-07 13:38:59 +0800

    Preparing release 1.0.0a13

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.0.0a13 (unreleased)
+1.0.0a13 (2013-11-07)
 ---------------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.0a13.dev0'
+version = '1.0.0a13'
 
 setup(name='ploneun.missions',
       version=version,
