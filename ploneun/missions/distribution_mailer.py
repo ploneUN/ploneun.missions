d42f639f99b707cbce98e34eed5fadb69b3faeee
glenn <glenn@glenn-Lenovo-G480>
2016-05-06 13:50:08 +0800

    removed pdb

--- ploneun/missions/distribution_mailer.py
+++ ploneun/missions/distribution_mailer.py
@@ -61,7 +61,6 @@ def send_distribution_list(obj, event):
     
     
     converter = getUtility(IPDFConverter)
-    import pdb; pdb.set_trace()
     pdf = converter.convert(obj)
 
     mailhost = obj.MailHost
